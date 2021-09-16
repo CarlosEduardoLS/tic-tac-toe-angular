f74a2485b00294028ec66c8d8504a95e7128eb34
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-15 22:33:35 -0300

    FEAT: Created new tic-tac-toe component

---
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tic-tac-toe',
+  templateUrl: './tic-tac-toe.component.html',
+  styleUrls: ['./tic-tac-toe.component.css'],
+})
+export class TicTacToeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
