34cd91d0a73633fc58074edbcd62cbc8564278ea
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-15 23:53:19 -0300

    FEAT: Implemented all logic to game works

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TicTacToeService } from './shared/tic-tac-toe.service';
 
 @Component({
   selector: 'app-tic-tac-toe',
@@ -6,7 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tic-tac-toe.component.css'],
 })
 export class TicTacToeComponent implements OnInit {
-  constructor() {}
+  constructor(private ticTacToeService: TicTacToeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.ticTacToeService.initialize();
+  }
+
+  get showStart(): boolean {
+    return this.ticTacToeService.showStart;
+  }
+
+  get showEnd(): boolean {
+    return this.ticTacToeService.showEnd;
+  }
+
+  get showBoard(): boolean {
+    return this.ticTacToeService.showBoard;
+  }
+
+  startGame(): void {
+    this.ticTacToeService.startGame();
+  }
+
+  play(posX: number, posY: number): void {
+    this.ticTacToeService.play(posX, posY);
+  }
+
+  showX(posX: number, posY: number): boolean {
+    return this.ticTacToeService.showX(posX, posY);
+  }
+
+  showO(posX: number, posY: number): boolean {
+    return this.ticTacToeService.showY(posX, posY);
+  }
+
+  showWin(posX: number, posY: number): boolean {
+    return this.ticTacToeService.showWin(posX, posY);
+  }
+
+  get player(): number {
+    return this.ticTacToeService.player;
+  }
+
+  newGame(): void {
+    this.ticTacToeService.newGame();
+  }
 }
