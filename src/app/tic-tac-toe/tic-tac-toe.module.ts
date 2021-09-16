a62aa7169ba1902487d7c5dd287b82027fabced0
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-15 22:37:19 -0300

    FIX: Included new provider on tic-tac-toe module

--- src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TicTacToeService } from './shared';
 import { TicTacToeComponent } from './tic-tac-toe.component';
 
 @NgModule({
   declarations: [TicTacToeComponent],
   imports: [CommonModule],
   exports: [TicTacToeComponent],
+  providers: [TicTacToeService],
 })
 export class TicTacToeModule {}
